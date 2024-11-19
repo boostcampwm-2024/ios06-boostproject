797bffad3998387fad00a7098b1041602caaf6f2
windowcow <bzbzkang@gmail.com>
2024-11-19 14:58:54 +0900

    chore:  PlaylistDetailViewModel 임시 저장 (#63)

--- Molio/Source/Presentation/PlaylistDetailViewModel.swift
+++ Molio/Source/Presentation/PlaylistDetailViewModel.swift
@@ -3,16 +3,49 @@ import Combine
 
 final class PlaylistDetailViewModel: ObservableObject {
     @Published var currentPlaylist: MolioPlaylist?
+    @Published var currentPlaylistMusics: [RandomMusic] = []
     
-    private var currentPlaylistSubscription: AnyCancellable?
+    private var subscriptions: Set<AnyCancellable> = []
+    
+    private let parallMusicFetchForISRCsUseCase: any ParallelMusicFetchForISRCsUseCase
     
     init(
-        playlistRepository: any CurrentPlaylistRepository = MockCurrentPlaylistRepository()
+        playlistRepository: any CurrentPlaylistRepository,
+        parallelMusicFetchForISRCsUseCase: any ParallelMusicFetchForISRCsUseCase
     ) {
-        self.currentPlaylistSubscription = playlistRepository.currentPlaylistPublisher
+        self.parallMusicFetchForISRCsUseCase = parallelMusicFetchForISRCsUseCase
+        self.setupCurrentPlaylistPublisher(currentPlaylistPublisher: playlistRepository.currentPlaylistPublisher)
+        self.setupCurrentPlaylistMusicsPublisher()
+    }
+    
+    func setupCurrentPlaylistPublisher(currentPlaylistPublisher: AnyPublisher<MolioPlaylist?, Never>) {
+        currentPlaylistPublisher
             .sink { [weak self] playlist in
                 self?.currentPlaylist = playlist
             }
+            .store(in: &subscriptions)
+    }
+    
+    func setupCurrentPlaylistMusicsPublisher() {
+        currentPlaylist.publisher
+            .flatMap { (currentPlaylist: MolioPlaylist) -> Future<[RandomMusic], Never> in
+                let isrcs = currentPlaylist.musicISRCs
+                
+                return Future { promise in
+                    Task { [weak self] in
+                        do {
+                            let musics = try await self?.parallMusicFetchForISRCsUseCase.execute(isrcs: isrcs)
+                            promise(.success(musics ?? []))
+                        } catch {
+                            // TODO: Error Handling
+                        }
+                    }
+                }
+            }
+            .sink { [weak self] musics in
+                self?.currentPlaylistMusics = musics
+            }
+            .store(in: &subscriptions)
     }
 }
 
