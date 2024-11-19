82e415c96d7049e1bc763878bb6c14517d9aa3a9
kybeen <rei980627@gmail.com>
2024-11-19 23:00:56 +0900

    style: DIContainer 설명 주석 추가 (#67)

--- Molio/Source/App/DIContainer.swift
+++ Molio/Source/App/DIContainer.swift
@@ -4,7 +4,7 @@ final class DIContainer {
     
     private var dependencies: [String: Any] = [:]
     
-    /// 의존성 객체를 등록
+    /// 의존성 등록
     ///  - Parameters
     ///     - `type`: 의존성 객체의 타입
     ///     - `dependency`: 의존성 객체 인스턴스
@@ -13,11 +13,14 @@ final class DIContainer {
         dependencies[key] = dependency
     }
     
+    /// 파라미터로 타입을 직접 지정하여 의존성 불러오기
     func resolve<T>(_ type: T.Type) -> T {
         let key = "\(type)"
         return dependencies[key] as! T
     }
     
+    /// 파라미터 없이 의존성 불러오기
+    /// - 할당받는 쪽에서 타입 지정 필요
     func resolve<T>() -> T {
         let key = "\(T.self)"
         return dependencies[key] as! T
