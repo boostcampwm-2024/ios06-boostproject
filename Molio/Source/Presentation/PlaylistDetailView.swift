34e4d73b807d2aff2b44cf9bc704466bf8fa8124
windowcow <bzbzkang@gmail.com>
2024-11-19 14:57:45 +0900

    chore: PlaylistDetailView 주석처리하기 (#63)
    - stash 하면 나중에 실수로 날아가는 위험성이 있는 것 같아서 일단은 주석 처리 해두었다.

--- Molio/Source/Presentation/PlaylistDetailView.swift
+++ Molio/Source/Presentation/PlaylistDetailView.swift
@@ -1,51 +1,62 @@
-import SwiftUI
-import Combine
-
-struct PlaylistDetailView: View {
-    @ObservedObject private var viewModel = PlaylistDetailViewModel()
-    @State private var isPlaylistChangeSheetPresented: Bool = false
-    
-    var body: some View {
-        List {
-            ForEach((0..<10).map { $0 }, id: \.self) { _ in
-                PlaylistDetailListItemView(music: .apt)
-                    .foregroundStyle(.white)
-                    .backgroundStyle(.clear)
-                    .listRowBackground(Color.clear)
-                    .listRowSeparatorTint(.gray)
-                    .listRowInsets(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 0))
-            }
-        }
-        .listStyle(.plain)
-        .background(Color.background)
-        .scrollContentBackground(.hidden)
-        .toolbar {
-            ToolbarItem(placement: .topBarLeading) {
-                Button {
-                    isPlaylistChangeSheetPresented.toggle()
-                } label: {
-                    Text(viewModel.currentPlaylist?.name ?? "제목 없음")
-                        .foregroundStyle(.white)
-                        .font(.title)
-                        .bold()
-                        .padding()
-                        .padding(.bottom, 5)
-                }
-            }
-        }
-        .toolbarBackground(
-            Color.background,
-            for: .navigationBar
-        )
-        .sheet(isPresented: $isPlaylistChangeSheetPresented) {
-            Text("힝 속았지")
-        }
-    }
-}
-
-#Preview {
-    NavigationStack {
-        PlaylistDetailView()
-    }
-    .navigationBarBackButtonHidden(false)
-}
+//import SwiftUI
+//import Combine
+//
+//struct PlaylistDetailView: View {
+//    @ObservedObject private var viewModel = PlaylistDetailViewModel(
+//        playlistRepository: MockCurrentPlaylistRepository(),
+//        parallelMusicFetchForISRCsUseCase: DefaultParallelMusicFetchForISRCsUseCase()
+//    )
+//    
+//    init(
+//        playlistRepository: some CurrentPlaylistRepository,
+//        parallelMusicFetchForISRCsUseCase: some ParallelMusicFetchForISRCsUseCase
+//    ) {
+//        self._viewModel = playlistRepository
+//    }
+//
+//    @State private var isPlaylistChangeSheetPresented: Bool = false
+//    
+//    var body: some View {
+//        List {
+//            ForEach(viewModel.currentPlaylist?.musicISRCs, id: \.self) { _ in
+//                PlaylistDetailListItemView(music: .apt)
+//                    .foregroundStyle(.white)
+//                    .backgroundStyle(.clear)
+//                    .listRowBackground(Color.clear)
+//                    .listRowSeparatorTint(.gray)
+//                    .listRowInsets(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 0))
+//            }
+//        }
+//        .listStyle(.plain)
+//        .background(Color.background)
+//        .scrollContentBackground(.hidden)
+//        .toolbar {
+//            ToolbarItem(placement: .topBarLeading) {
+//                Button {
+//                    isPlaylistChangeSheetPresented.toggle()
+//                } label: {
+//                    Text(viewModel.currentPlaylist?.name ?? "제목 없음")
+//                        .foregroundStyle(.white)
+//                        .font(.title)
+//                        .bold()
+//                        .padding()
+//                        .padding(.bottom, 5)
+//                }
+//            }
+//        }
+//        .toolbarBackground(
+//            Color.background,
+//            for: .navigationBar
+//        )
+//        .sheet(isPresented: $isPlaylistChangeSheetPresented) {
+//            Text("힝 속았지")
+//        }
+//    }
+//}
+//
+//#Preview {
+//    NavigationStack {
+//        PlaylistDetailView()
+//    }
+//    .navigationBarBackButtonHidden(false)
+//}
