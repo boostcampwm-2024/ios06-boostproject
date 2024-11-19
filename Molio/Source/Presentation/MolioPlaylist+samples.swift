b338846e945e14d6647f06eaa448f35b6df66c81
windowcow <bzbzkang@gmail.com>
2024-11-19 10:56:37 +0900

    feat: MolioPlaylist의 sample들 만들기  (#63)

---
+++ Molio/Source/Presentation/MolioPlaylist+samples.swift
@@ -0,0 +1,21 @@
+import Foundation
+import SwiftUI
+import Combine
+
+extension MolioPlaylist {
+    static let samples: [MolioPlaylist] = (1...10).map { index in
+        MolioPlaylist(
+            id: UUID(),
+            name: "Playlist \(index)",
+            createdAt: Date().addingTimeInterval(-Double(index * 86400)), // 하루씩 과거 날짜
+            musicISRCs: [
+                "USUM72021083", // "Peaches" by Justin Bieber
+                "USSM12100668", // "Leave The Door Open" by Bruno Mars
+                "USRC12003145", // "Save Your Tears" by The Weeknd
+                "GBAHS2100386", // "Bad Habits" by Ed Sheeran
+                "USUG12101234"  // "Stay" by The Kid LAROI & Justin Bieber
+            ],
+            filters: ["Filter \(index)", "Genre \((index % 3) + 1)"] // 필터 임의 생성
+        )
+    }
+}
