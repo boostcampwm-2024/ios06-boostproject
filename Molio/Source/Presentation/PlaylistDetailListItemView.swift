e42dca26f24313bb02948c9a6d17935aeffbc3b2
windowcow <bzbzkang@gmail.com>
2024-11-19 10:58:23 +0900

    feat: 플레이리스트의 노래를 보여주는 PlaylistDetailListItemView 정의 (#63)

---
+++ Molio/Source/Presentation/PlaylistDetailListItemView.swift
@@ -0,0 +1,38 @@
+import Foundation
+import SwiftUI
+
+struct PlaylistDetailListItemView: View {
+    var music: RandomMusic
+    
+    var body: some View {
+        HStack(spacing: 15) {
+            AsyncImage(url: music.artworkImageURL) { phase in
+                phase.image?
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(.rect(cornerRadius: 5))
+
+            }
+
+            VStack(alignment: .leading) {
+                Text(music.title)
+                Text(music.artistName)
+                    .font(.footnote)
+                    .fontWeight(.ultraLight)
+            }
+            
+            Spacer()
+        }
+        .frame(
+            minWidth: 0,
+            maxWidth: .infinity,
+            minHeight: 0,
+            maxHeight: 60
+        )
+    }
+}
+
+#Preview {
+    PlaylistDetailListItemView(music: RandomMusic.apt)
+}
+
