46f647163fb99a4a4bf359152bbfaec9f37052ed
Park-kxng <coding.parkgy29@gmail.com>
2024-11-20 22:53:30 +0900

    feat: 선택된 음악목록의 배경색 지정 (#87)

---
+++ Molio/Source/Presentation/PlaylistDetail/View/PlaylistDetailListView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct PlaylistDetailListView: View {
+    @Binding private var musics: [MolioMusic]
+    @Binding private var selectedIndex: Int? // 선택된 인덱스
+
+    init(musics: Binding<[MolioMusic]>, selectedIndex: Binding<Int?>) {
+        self._musics = musics
+        self._selectedIndex = selectedIndex
+    }
+
+    var body: some View {
+        List(musics.indices, id: \.self) { index in
+            let music = musics[index]
+            
+            PlaylistDetailListItemView(music: music)
+                .listRowBackground(selectedIndex == index ? .main.opacity(0.2) : Color.clear)
+                .listRowSeparatorTint(.gray)
+                .listRowInsets(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 0))
+                .onTapGesture {
+                    selectedIndex = index
+                }
+        }
+        .listStyle(.plain)
+        .scrollContentBackground(.hidden)
+    }
+}
+#Preview {
+    ZStack {
+        Color.black
+        PlaylistDetailListView(
+            musics: .constant(
+                MolioMusic.all
+            ),
+            selectedIndex: .constant(0)
+        )
+        .foregroundStyle(.white)
+        .background(Color.background)
+    }
+}
