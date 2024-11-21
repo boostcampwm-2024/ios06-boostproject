0b5bb43dc31f97f06cdf9b7109fac3c1e430bdc6
kybeen <rei980627@gmail.com>
2024-11-21 22:17:13 +0900

    feat: MolioPlaylist에 필터 정보 업데이트 메서드 구현 (#129)

--- Molio/Source/Domain/Entity/PlaylistEntity.swift
+++ Molio/Source/Domain/Entity/PlaylistEntity.swift
@@ -6,4 +6,14 @@ struct MolioPlaylist {
     let createdAt: Date
     let musicISRCs: [String]
     let filter: MusicFilter
+    
+    func updateFilter(to newFilter: MusicFilter) -> Self {
+        return MolioPlaylist(
+            id: self.id,
+            name: self.name,
+            createdAt: self.createdAt,
+            musicISRCs: self.musicISRCs,
+            filter: newFilter
+        )
+    }
 }
