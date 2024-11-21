e1f6396668fea8c4690bc316ec829494994bde69
windowcow <bzbzkang@gmail.com>
2024-11-21 21:44:58 +0900

    feat: MusicListView 스와이프 기능 구현 (#103)

--- Molio/Source/Presentation/PlaylistDetail/View/MusicListView.swift
+++ Molio/Source/Presentation/PlaylistDetail/View/MusicListView.swift
@@ -17,36 +17,36 @@ struct MusicListView: View {
     }
 
     var body: some View {
-        List(musics.indices, id: \.self) { index in
-            let music = musics[index]
-            
-            MusicCellView(music: music)
-                .listRowBackground(selectedIndex == index ? .main.opacity(0.2) : Color.clear)
-                .listRowSeparatorTint(.gray)
-                .listRowInsets(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 0))
-                .onTapGesture {
-                    selectedIndex = index
-                }
-                .swipeActions {
-                    Button(role: .destructive) {
-                        withAnimation {
+        List {
+            ForEach(musics.indices, id: \.self) { index in
+                let music = musics[index]
+                
+                MusicCellView(music: music)
+                    .listRowBackground(selectedIndex == index ? .main.opacity(0.2) : Color.clear)
+                    .listRowSeparatorTint(.gray)
+                    .listRowInsets(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 0))
+                    .onTapGesture {
+                        selectedIndex = index
+                    }
+                    .swipeActions {
+                        Button(role: .destructive) {
                             removeTargetMusic = music
                             isAlertPresenting.toggle()
+                        } label: {
+                            Label("Delete", systemImage: "trash")
                         }
-                    } label: {
-                        Label("Delete", systemImage: "trash")
                     }
-                    
-                }
+            }
         }
         .listStyle(.plain)
         .scrollContentBackground(.hidden)
         .alert("정말 삭제하시겠습니까?", isPresented: $isAlertPresenting, presenting: removeTargetMusic) { music in
             // 삭제 확인 버튼
-            Button("삭제", role: .destructive) {
+            Button("삭제") {
                 deleteMusic(music: music)
                 removeTargetMusic = nil
             }
+            .tint(.red)
             // 취소 버튼
             Button("취소", role: .cancel) {
                 // 취소 시 추가 작업이 필요하다면 여기에 작성
