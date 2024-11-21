43cd0f9ac0aa513295ff1cb703e28f8b64e8331f
kybeen <rei980627@gmail.com>
2024-11-21 22:58:23 +0900

    feat: MusicFilterViewModel 로직 작성 (#129)
    - 초기화 시 현재 플레이리스트 정보를 불러옴
      - 현재 플레이리스트와
      - 설정된 장르를 저장
    - 저장 버튼 클릭 시 유즈케이스 메서드 호출

--- Molio/Source/Presentation/MusicFilter/ViewModel/MusicFilterViewModel.swift
+++ Molio/Source/Presentation/MusicFilter/ViewModel/MusicFilterViewModel.swift
@@ -6,7 +6,8 @@ final class MusicFilterViewModel: ObservableObject {
     private let publishCurrentPlaylistUseCase: PublishCurrentPlaylistUseCase
     private let updatePlaylistUseCase: UpdatePlaylistUseCase
     
-    @Published private(set) var allGenres: [MusicGenre] // TODO: - 전체 장르 시드 불러오기
+    @Published private(set) var allGenres: [MusicGenre]
+    @Published private(set) var currentPlaylist: MolioPlaylist?
     @Published private(set) var selectedGenres: Set<MusicGenre>
     
     private var subscriptions = Set<AnyCancellable>()
@@ -16,18 +17,19 @@ final class MusicFilterViewModel: ObservableObject {
         publishCurrentPlaylistUseCase: PublishCurrentPlaylistUseCase = MockPublishCurrentPlaylistUseCase(), // TODO: - 의존성 주입
         updatePlaylistUseCase: UpdatePlaylistUseCase = DIContainer.shared.resolve(),
         allGenres: [MusicGenre] = MusicGenre.allCases,
-//        selectedGenres: Set<MusicGenre> = []
+        selectedGenres: Set<MusicGenre> = []
     ) {
         self.fetchAvailableGenresUseCase = fetchAvailableGenresUseCase
         self.publishCurrentPlaylistUseCase = publishCurrentPlaylistUseCase
         self.updatePlaylistUseCase = updatePlaylistUseCase
         self.allGenres = allGenres
-//        self.selectedGenres = selectedGenres
+        self.selectedGenres = selectedGenres
         
 //        getAllGenres() // TODO: - 호출 횟수 조절
-        bindCurrentFilter()
+        bindCurrentPlaylist()
     }
     
+    /// 장르 선택 처리
     func toggleSelection(of genre: MusicGenre) {
         if selectedGenres.contains(genre) {
             selectedGenres.remove(genre)
@@ -36,25 +38,44 @@ final class MusicFilterViewModel: ObservableObject {
         }
     }
     
-    func getAllGenres() {
+    /// 모든 장르 종류 불러오기
+    private func getAllGenres() {
         Task { @MainActor in
             allGenres = try await fetchAvailableGenresUseCase.execute()
         }
     }
     
-    func bindCurrentFilter() {
+    /// 현재 필터 정보 구독
+    /// - 현재 플레이리스트
+    /// - 현재 플레이리스트에서 선택된 장르들
+    private func bindCurrentPlaylist() {
         publishCurrentPlaylistUseCase.execute()
             .sink { [weak self] playlist in
-                self?.allGenres = playlist?.filter.genres ?? []
+                self?.currentPlaylist = playlist
+                self?.selectedGenres = Set<MusicGenre>(playlist?.filter.genres ?? [])
             }
             .store(in: &subscriptions)
     }
     
-    func saveGenreSelection() {
-        
+    /// 장르 설정 정보 저장
+    private func saveGenreSelection() throws {
+        guard let currentPlaylist = currentPlaylist else { return }
+        let newFilter = MusicFilter(genres: Array(selectedGenres))
+        let updatedPlaylist = currentPlaylist.updateFilter(to: newFilter)
+        Task {
+            try await updatePlaylistUseCase.execute(id: currentPlaylist.id, to: updatedPlaylist)
+        }
     }
 }
 
-protocol UpdatePlaylistUseCase {
-    func execute(id: UUID, to updatedPlaylist: MolioPlaylist) async throws
+// MARK: - Delegate
+
+extension MusicFilterViewModel: MusicFilterViewControllerDelegate {
+    func didSaveButtonTapped() {
+        do {
+            try saveGenreSelection()
+        } catch {
+            // TODO: - 에러 처리
+        }
+    }
 }
