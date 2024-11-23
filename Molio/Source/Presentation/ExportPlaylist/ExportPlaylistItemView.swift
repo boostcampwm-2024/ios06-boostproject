57c17260be91a1347b192619b4fbc6ece1e04806
Minsoo Kang <alstn38@naver.com>
2024-11-23 18:41:47 +0900

    refactor: model 타입 변경 및 Image 타입 변경 (#133)

--- Molio/Source/Presentation/ExportPlaylist/ExportPlaylistItemView.swift
+++ Molio/Source/Presentation/ExportPlaylist/ExportPlaylistItemView.swift
@@ -1,40 +1,37 @@
 import SwiftUI
 
 struct ExportPlaylistItemView: View {
-    var music: MolioMusic
+    var musicItems: ExportMusicItem
     
-    init(music: MolioMusic) {
-        self.music = music
+    init(musicItems: ExportMusicItem) {
+        self.musicItems = musicItems
     }
     
     var body: some View {
         HStack(spacing: 15) {
-            AsyncImage(url: music.artworkImageURL) { phase in
-                if let image = phase.image {
-                    image
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 42, height: 42)
-                        .padding(.top, 6)
-                        .padding(.bottom, 6)
-                        .padding(.leading, 16)
-                } else {
-                    Color.background
-                        .frame(width: 42, height: 42)
-                        .padding(.top, 6)
-                        .padding(.bottom, 6)
-                        .padding(.leading, 16)
-                }
+            if let imageData = musicItems.artworkImageData,
+               let uiImage = UIImage(data: imageData) {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 42, height: 42)
+                    .padding(.top, 6)
+                    .padding(.bottom, 6)
+                    .padding(.leading, 16)
+            } else {
+                Color.background
+                    .frame(width: 42, height: 42)
+                    .padding(.top, 6)
+                    .padding(.bottom, 6)
+                    .padding(.leading, 16)
             }
-            
             VStack(alignment: .leading) {
-                Text(music.title)
+                Text(musicItems.title)
                     .font(.body)
-                Text(music.artistName)
+                Text(musicItems.artistName)
                     .font(.footnote)
                     .fontWeight(.ultraLight)
             }
-            
             Spacer()
         }
         .frame(
@@ -44,10 +41,4 @@ struct ExportPlaylistItemView: View {
             maxHeight: 56
         )
     }
-}
-
-#Preview {
-    ExportPlaylistItemView(music: MolioMusic.apt)
-        .background(Color.white)
-        .foregroundStyle(.black)
 }
