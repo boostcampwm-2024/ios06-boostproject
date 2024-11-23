cd5ff091ca046590e54517d4c0323a8981269a3b
kybeen <rei980627@gmail.com>
2024-11-23 20:01:53 +0900

    refactor: MockPlaylistRepository 수정 (#129)
    - 업데이트 메서드 추가
    - PlaylistRepository 인터페이스 순서와 맞춤

--- MolioTests/Test Double/Mock/Repository/MockPlaylistRepository.swift
+++ MolioTests/Test Double/Mock/Repository/MockPlaylistRepository.swift
@@ -3,29 +3,35 @@ import Foundation
 @testable import Molio
 
 final class MockPlaylistRepository: PlaylistRepository {
-    private var playlists = CurrentValueSubject<[MolioPlaylist], Never>([])
+    var mockPlaylist: [MolioPlaylist] = []
+    var willThrowError: Bool = false
     
     var playlistsPublisher: AnyPublisher<[MolioPlaylist], Never> {
-        playlists.eraseToAnyPublisher()
+        return Just(mockPlaylist).eraseToAnyPublisher()
     }
     
     func addMusic(isrc: String, to playlistName: String) {}
     
     func deleteMusic(isrc: String, in playlistName: String) {}
     
-    func deletePlaylist(_ playlistName: String) {}
+    func moveMusic(isrc: String, in playlistName: String, fromIndex: Int, toIndex: Int) {}
     
-    func fetchPlaylist(for name: String) -> MolioPlaylist? {
-        MolioPlaylist(id: UUID(), name: name, createdAt: Date.now, musicISRCs: [], filter: MusicFilter(genres: []))
+    func saveNewPlaylist(_ playlistName: String) async throws -> UUID {
+        guard !willThrowError else { throw CoreDataError.saveFailed }
+        return UUID()
     }
+    
+    func deletePlaylist(_ playlistName: String) {}
     
     func fetchPlaylists() -> [MolioPlaylist]? {
         []
     }
     
-    func moveMusic(isrc: String, in playlistName: String, fromIndex: Int, toIndex: Int) {}
+    func fetchPlaylist(for name: String) -> MolioPlaylist? {
+        MolioPlaylist(id: UUID(), name: name, createdAt: Date.now, musicISRCs: [], filter: MusicFilter(genres: []))
+    }
     
-    func saveNewPlaylist(_ playlistName: String) async throws -> UUID {
-        return UUID()
+    func updatePlaylist(id: UUID, to newValue: Molio.MolioPlaylist) async throws {
+        guard !willThrowError else { throw CoreDataError.updateFailed }
     }
 }
