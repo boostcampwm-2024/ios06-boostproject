85b7323fdb63edea9189a0f5859d1cecf8bca911
Minsoo Kang <alstn38@naver.com>
2024-11-25 16:10:18 +0900

    feat: 로그인 상태 관리 UseCase 구현 (#101)

---
+++ Molio/Source/Domain/UseCase/ManageAuthenticationUseCase/DefaultManageAuthenticationUseCase.swift
@@ -0,0 +1,20 @@
+struct DefaultManageAuthenticationUseCase: ManageAuthenticationUseCase {
+    private let authStateRepository: AuthStateRepository
+    
+    init(authStateRepository: AuthStateRepository = DIContainer.shared.resolve()) {
+        self.authStateRepository = authStateRepository
+    }
+    
+    func isAuthModeSelected() -> Bool {
+        return authStateRepository.authSelection == .selected
+    }
+    
+    func setAuthMode(_ mode: AuthMode) {
+        authStateRepository.setAuthMode(mode)
+        setAuthSelection(.selected)
+    }
+    
+    private func setAuthSelection(_ selection: AuthSelection) {
+        authStateRepository.setAuthSelection(selection)
+    }
+}
