2755e25e51d6707d16d01f4b0df06b3bc4638f4f
Minsoo Kang <alstn38@naver.com>
2024-11-26 18:05:55 +0900

    feat: SettingTextItemView UI 구현 (#185)

---
+++ Molio/Source/Presentation/Setting/SettingTextItemView.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct SettingTextItemView: View {
+    let titleText: String
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                Text(titleText)
+                    .font(.pretendardRegular(size: 16))
+                    .foregroundStyle(.white)
+                    .padding(.leading, 16)
+                    .padding(.top, 11)
+                    .padding(.bottom, 11)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+                    .foregroundColor(.gray)
+                    .padding(.trailing, 16)
+            }
+            Divider()
+                .background(Color(UIColor.darkGray))
+                .padding(.leading, 16)
+                .padding(.bottom, 1)
+        }
+        .background(Color(uiColor: UIColor(resource: .background)))
+    }
+}
