00e10cee9fa18f1247928f9cadf624793ae75765
kybeen <rei980627@gmail.com>
2024-11-26 20:10:31 +0900

    feat: AddMusicToPlaylistUseCase 유즈케이스 추가 (#182)

---
+++ Molio/Source/Domain/UseCase/AddMusicToPlaylistUseCase/DefaultAddMusicToPlaylistUseCase.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+struct DefaultAddMusicToPlaylistUseCase: AddMusicToPlaylistUseCase {
+    private let playlistRepository: PlaylistRepository
+    
+    init(
+        playlistRepository: PlaylistRepository = DIContainer.shared.resolve()
+    ) {
+        self.playlistRepository = playlistRepository
+    }
+    
+    func execute(isrc: String, to playlistID: UUID) async throws {
+        print(#fileID, #function)
+        try await playlistRepository.addMusic(isrc: isrc, to: playlistID)
+    }
+}
