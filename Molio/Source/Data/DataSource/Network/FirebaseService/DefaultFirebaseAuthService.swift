a15103868875fe3007756ec20a2ce5999e5693e7
Park-kxng <coding.parkgy29@gmail.com>
2024-11-26 23:17:42 +0900

    feat: DefaultFirebaseAuthService 추가 구현 (#192)

---
+++ Molio/Source/Data/DataSource/Network/FirebaseService/DefaultFirebaseAuthService.swift
@@ -0,0 +1,49 @@
+import FirebaseAuth
+
+struct DefaultFirebaseAuthService: AuthService {
+    
+    func getCurrentUser() throws -> User? {
+        guard let currentUser = Auth.auth().currentUser else {
+            throw FirebaseAuthError.userNotFound
+        }
+        return currentUser
+    }
+    
+    func signInApple(info: AppleAuthInfo) async throws {
+        let credential = OAuthProvider.appleCredential(
+            withIDToken: info.idToken,
+            rawNonce: info.nonce,
+            fullName: info.fullName
+        )
+        do {
+            try await Auth.auth().signIn(with: credential)
+        } catch {
+            throw FirebaseAuthError.loginFailed
+        }
+    }
+    
+    func logout() throws {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            throw FirebaseAuthError.logoutFailed
+        }
+    }
+    
+    func deleteAccount() async throws {
+        guard let currentUser = Auth.auth().currentUser else {
+            throw FirebaseAuthError.userNotFound
+        }
+        
+        do {
+            try await currentUser.delete()
+        } catch let error as NSError {
+            if error.code == AuthErrorCode.requiresRecentLogin.rawValue {
+                throw FirebaseAuthError.requiresReauthentication
+            } else {
+                throw FirebaseAuthError.deleteAccountFailed
+            }
+        }
+    }
+    
+}
