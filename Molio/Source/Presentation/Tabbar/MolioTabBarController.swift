b270303aba9015bc218ece97d57b2147b0f9f86e
Minsoo Kang <alstn38@naver.com>
2024-11-26 12:12:06 +0900

    refactor: TabBar community navigation Controller 제거 (#170)

--- Molio/Source/Presentation/Tabbar/MolioTabBarController.swift
+++ Molio/Source/Presentation/Tabbar/MolioTabBarController.swift
@@ -56,15 +56,14 @@ final class MolioTabBarController: UITabBarController {
         return swipeMusicViewNavigationController
     }
     
-    private func createCommunityViewController() -> UINavigationController {
+    private func createCommunityViewController() -> UIViewController {
         let communityViewController = CommunityViewController()
-        let communityViewNavigationController = UINavigationController(rootViewController: communityViewController)
         communityViewController.tabBarItem = UITabBarItem(
             title: "",
             image: UIImage(resource: .personCircle).withRenderingMode(.alwaysOriginal),
             selectedImage: UIImage(resource: .personCircle).withRenderingMode(.alwaysOriginal)
         )
         
-        return communityViewNavigationController
+        return communityViewController
     }
 }
