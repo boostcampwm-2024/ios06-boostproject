6c3ed44de7a719d7d418cf705d4b89225e6d7330
kybeen <rei980627@gmail.com>
2024-11-28 02:04:21 +0900

    feat: BasicButton ButtonType 추가 (#175)

--- Molio/Source/Presentation/Common/Component/BasicButton.swift
+++ Molio/Source/Presentation/Common/Component/BasicButton.swift
@@ -4,15 +4,19 @@ enum ButtonType: String {
     case cancel = "취소"
     case confirm = "완료"
     case next = "다음"
+    case exportToAppleMusic = "애플 뮤직 플레이리스트 내보내기"
+    case exportToImage = "플레이리스트 이미지로 내보내기"
     case goToAppleMusic = "애플 뮤직으로 들으러 가기"
     case saveImage = "전체 이미지 저장"
     case shareInstagram = "인스타 공유"
+    case needAppleMusicSubcription = "Apple Music 구독 후 이용 가능합니다."
+    case didNotSelectPlatform = "플랫폼을 선택해주세요"
 }
 
 struct BasicButton: View {
     var type: ButtonType
     var isEnabled: Bool = true
-    var action: () -> Void
+    var action: () -> Void = {}
     
     var body: some View {
         Button(action: action) {
@@ -31,9 +35,9 @@ struct BasicButton: View {
     // 버튼의 배경색을 타입에 따라 변경
     private var backgroundColor: Color {
         switch type {
-        case .cancel:
+        case .cancel, .needAppleMusicSubcription, .didNotSelectPlatform:
             return Color.white.opacity(0.2)
-        case .confirm, .next, .goToAppleMusic, .saveImage, .shareInstagram:
+        case .confirm, .next, .exportToAppleMusic, .exportToImage, .goToAppleMusic, .saveImage, .shareInstagram:
             return Color.mainLighter
         }
         
@@ -41,9 +45,9 @@ struct BasicButton: View {
     
     private var textColor: Color {
         switch type {
-        case .cancel:
+        case .cancel, .needAppleMusicSubcription, .didNotSelectPlatform:
             return Color.white
-        case .confirm, .next, .goToAppleMusic, .saveImage, .shareInstagram:
+        case .confirm, .next, .exportToAppleMusic, .exportToImage, .goToAppleMusic, .saveImage, .shareInstagram:
             return Color.black
         }
     }
