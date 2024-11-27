a83886837381345b5286a20275ac3984686c5fb6
Park-kxng <coding.parkgy29@gmail.com>
2024-11-27 16:14:20 +0900

    feat: FirebaseUserService 구현 및 테스트 코드 추가 (#201)

---
+++ Molio/Source/Data/DataSource/Network/FirebaseService/FirebaseUserService.swift
@@ -0,0 +1,69 @@
+import FirebaseStorage
+import FirebaseCore
+import FirebaseFirestore
+
+final class FirebaseUserService: UserService {
+    private let storageManager: FirebaseStorageManager
+    private let db: Firestore
+    private let collectionName: String = "users"
+    init(
+        storageManager: FirebaseStorageManager,
+        db: Firestore
+    ) {
+        self.storageManager = storageManager
+        self.db = db
+        
+        // FirebaseApp이 이미 초기화되었는지 확인
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
+        
+    }
+    
+    func createUser(_ user: MolioUserDTO) async throws {
+        guard let userData = user.toDictionary() else { return }
+        
+        let docRef = getDocumentReference(documentName: user.id)
+        try await docRef.setData(userData)
+    }
+    
+    func readUser(userID: String) async throws -> MolioUserDTO {
+        let docRef = getDocumentReference(documentName: userID)
+        return try await docRef.getDocument(as: MolioUserDTO.self)
+    }
+    
+    func updateUser(_ user: MolioUserDTO) async throws {
+        guard let updateUserData = user.toDictionary() else { return }
+        let docRef = getDocumentReference(documentName: user.id)
+
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
+            docRef.updateData(updateUserData) { error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume(returning: ())
+                }
+            }
+        }
+    }
+    
+    func deleteUser(userID: String) async throws {
+        let docRef = getDocumentReference(documentName: userID)
+        
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
+            docRef.delete { error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume()
+                }
+            }
+        }
+    }
+    
+    // MARK: - Private Method
+    
+    private func getDocumentReference(documentName: String) -> DocumentReference {
+        return db.collection(collectionName).document(documentName)
+    }
+}
