cf84b1dabe0f6adb27c1d190846ee423ef07115d
Park-kxng <coding.parkgy29@gmail.com>
2024-11-27 16:00:59 +0900

    refactor: UserService 함수 수정 (#201)

--- Molio/Source/Data/DataSource/Network/FirebaseService/Protocol/UserService.swift
+++ Molio/Source/Data/DataSource/Network/FirebaseService/Protocol/UserService.swift
@@ -2,5 +2,5 @@ protocol UserService {
     func createUser(_ user: MolioUserDTO) async throws
     func readUser(userID: String) async throws -> MolioUserDTO
     func updateUser(_ user: MolioUserDTO) async throws
-    func deleteUser(_ user: MolioUserDTO) async throws
+    func deleteUser( userID: String) async throws
 }
