5232929b23f780e4ae69e058618b022e40bc17dc
windowcow <bzbzkang@gmail.com>
2024-11-27 17:31:56 +0900

    chore: 임시로 만들어둔 MolioUserDTO 삭제 (#206)

--- Molio/Source/Data/DataSource/Network/FirestorePlaylistService.swift
+++ Molio/Source/Data/DataSource/Network/FirestorePlaylistService.swift
@@ -69,10 +69,3 @@ final class FirestorePlaylistService: PlaylistService {
         return db.collection(collectionName).document(documentName)
     }
 }
-
-struct MolioUserDTO: Codable {
-    let id: String          // 유저의 고유 ID
-    var name: String      // 유저 닉네임
-    var profileImageURL: URL? // 유저의 사진 URL
-    var description: String?  // 유저의 한 줄 설명
-}
