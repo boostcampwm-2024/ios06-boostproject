05abdffba1317232fba9a5105f31995cf934c648
Park-kxng <coding.parkgy29@gmail.com>
2024-11-27 16:13:56 +0900

    feat: MolioUserDTO toDictionary() 추가 (#201)

--- Molio/Source/Data/Model/DTO/MolioUserDTO.swift
+++ Molio/Source/Data/Model/DTO/MolioUserDTO.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 struct MolioUserDTO: Codable {
     let id: String                // 유저의 고유 ID
     var name: String              // 유저 닉네임
@@ -9,5 +11,14 @@ struct MolioUserDTO: Codable {
         case name
         case profileImageURL
         case description
+    }
+    
+    func toDictionary() -> [String: Any]? {
+        guard let data = try? JSONEncoder().encode(self),
+              let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
+              let dictionary = jsonObject as? [String: Any] else {
+            return nil
+        }
+        return dictionary
     }
 }
