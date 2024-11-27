e53f18e2e7e4e0ecc60d4dd38f8d051578c30b4e
windowcow <bzbzkang@gmail.com>
2024-11-27 17:13:12 +0900

    feat: FirestorePlaylistService 구현 (#206)

---
+++ Molio/Source/Data/DataSource/Network/FirestorePlaylistService.swift
@@ -0,0 +1,78 @@
+import FirebaseCore
+import FirebaseFirestore
+
+final class FirestorePlaylistService: PlaylistService {
+    private let db: Firestore
+    private let collectionName = "playlists"
+    
+    init(
+        db: Firestore = Firestore.firestore()
+        
+    ) {
+        self.db = db
+        
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
+    }
+    
+    func createPlaylist(playlist: MolioPlaylistDTO) async throws {
+        let docRef = getDocumentReference(documentName: playlist.id)
+
+        return try await withCheckedThrowingContinuation { continuation in
+            do {
+                try docRef.setData(from: playlist) { error in
+                    if let error {
+                        continuation.resume(throwing: error)
+                    } else {
+                        continuation.resume(returning: ())
+                    }
+                }
+            } catch {
+                continuation.resume(throwing: error)
+            }
+        }
+    }
+    
+    func readPlaylist(playlistID: UUID) async throws -> MolioPlaylistDTO {
+        let docRef = getDocumentReference(documentName: playlistID.uuidString)
+
+        return try await docRef.getDocument(as: MolioPlaylistDTO.self)
+    }
+    
+    func readAllPlaylist(userID: String) async throws -> [MolioPlaylistDTO] {
+        
+        try await db.collection(collectionName)
+            .whereField("authorID", isEqualTo: userID)
+            .getDocuments()
+            .documents
+            .compactMap { try? $0.data(as: MolioPlaylistDTO.self) }
+    }
+
+    func updatePlaylist(newPlaylist: MolioPlaylistDTO) async throws {
+        let docRef = getDocumentReference(documentName: newPlaylist.id)
+        
+        guard let dictionary = newPlaylist.toDictionary() else {
+            throw FirestoreError.failedToConvertToDictionary
+        }
+        
+        return try await docRef.updateData(dictionary)
+    }
+    
+    func deletePlaylist(playlistID: UUID) async throws {
+        let docRef = db.collection(collectionName).document(playlistID.uuidString)
+
+        return try await docRef.delete()
+    }
+    
+    private func getDocumentReference(documentName: String) -> DocumentReference {
+        return db.collection(collectionName).document(documentName)
+    }
+}
+
+struct MolioUserDTO: Codable {
+    let id: String          // 유저의 고유 ID
+    var name: String      // 유저 닉네임
+    var profileImageURL: URL? // 유저의 사진 URL
+    var description: String?  // 유저의 한 줄 설명
+}
