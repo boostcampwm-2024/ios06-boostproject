11846fc9dc3a603b70d046d00c0081d487279469
Park-kxng <coding.parkgy29@gmail.com>
2024-11-28 17:33:55 +0900

    feat: CurrentUserIdUseCase 프로토콜 및 구현체 정의 복구
    This reverts commit 351b8da3b46aded150f653aed6daccdc597b9deb.

--- Molio/Source/Domain/Entity/MolioUser.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct MolioUser: Identifiable {
-    let id: String          // 유저의 고유 ID
-    var name: String      // 유저 닉네임
-    var profileImageURL: URL? // 유저의 사진 URL
-    var description: String?  // 유저의 한 줄 설명
-}
