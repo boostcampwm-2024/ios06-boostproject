e8b4e647d71bb41055fe2c0d2b72d0e817027649
kybeen <rei980627@gmail.com>
2024-11-28 17:56:33 +0900

    feat: MusicKitService 애플뮤직 플레이리스트 내보내기 메서드 구현  (#176)
    - 인터페이스, 구현체 작성
      - DefaultMusicKitService에서 아래 3가지 과정을 거쳐서 플레이리스트를 생성함
        - 플레이리스트 생성 -> 각 노래 검색 -> 검색한 노래를 플레이리스트에 추가
    - MusicKitError에 플레이리스트 생성 실패 에러 case 추가

--- Molio/Source/Data/DataSource/Network/MusicKitService/DefaultMusicKitService.swift
+++ Molio/Source/Data/DataSource/Network/MusicKitService/DefaultMusicKitService.swift
@@ -48,6 +48,21 @@ struct DefaultMusicKitService: MusicKitService {
 
     }
     
+    /// 애플 뮤직 플레이리스트 내보내기
+    func exportAppleMusicPlaylist(name: String, isrcs: [String]) async throws {
+        guard checkAuthorizationStatus() else { return }
+        
+        let playlist = try await createPlaylist(name: name)
+        for isrc in isrcs {
+            guard let song = await searchSong(with: isrc) else { continue }
+            do {
+                try await addSong(song, to: playlist)
+            } catch {
+                continue
+            }
+        }
+    }
+    
     /// 애플 뮤직 접근 권한 상태를 확인
     private func checkAuthorizationStatus() -> Bool {
         switch MusicAuthorization.currentStatus {
@@ -68,5 +83,36 @@ struct DefaultMusicKitService: MusicKitService {
     /// 애플 뮤직 접근 권한 요청
     private func requestAuthorization() async {
         _ = await MusicAuthorization.request()
+    }
+    
+    /// 플레이리스트 생성
+    /// - 생성에 실패한 경우 `MusicKitError.failedToCreatePlaylist` 에러를 throw
+    private func createPlaylist(name: String) async throws -> MusicKit.Playlist {
+        do {
+            return try await MusicLibrary.shared.createPlaylist(name: name)
+        } catch {
+            throw MusicKitError.failedToCreatePlaylist(name: name)
+        }
+    }
+    
+    /// 플레이리스트에 노래 추가
+    private func addSong(_ song: Song, to playlist: MusicKit.Playlist) async throws {
+        try await MusicLibrary.shared.add(song, to: playlist)
+        print("\(song.title)를 \(playlist.name)에 추가함")
+    }
+    
+    /// 애플 뮤직에서 isrc 값으로 검색한 노래를 `MusicKit.Song` 타입으로 반환
+    /// - 찾지 못한 경우 `nil`을 반환
+    private func searchSong(with isrc: String) async -> Song? {
+        let request = MusicCatalogResourceRequest<Song>(matching: \.isrc, equalTo: isrc)
+        do {
+            let response = try await request.response()
+            guard let searchedMusic = response.items.first else {
+                return nil
+            }
+            return searchedMusic
+        } catch {
+            return nil
+        }
     }
 }
--- Molio/Source/Data/DataSource/Network/MusicKitService/MusicKitError.swift
+++ Molio/Source/Data/DataSource/Network/MusicKitService/MusicKitError.swift
@@ -5,6 +5,7 @@ enum MusicKitError: LocalizedError {
     case restricted
     case failedSubscriptionCheck
     case didNotSubscribe
+    case failedToCreatePlaylist(name: String)
     
     var errorDescription: String? {
         switch self {
@@ -12,6 +13,7 @@ enum MusicKitError: LocalizedError {
         case .restricted: "현재 디바이스에서 Apple Music 서비스를 이용할 수 없습니다."
         case .failedSubscriptionCheck: "Apple Music 구독 여부 확인에 실패했습니다."
         case .didNotSubscribe: "Apple Music을 구독하고 있지 않습니다."
+        case .failedToCreatePlaylist(let name): "[\(name)] 플레이리스트를 생성하는데 실패했습니다."
         }
     }
 }
