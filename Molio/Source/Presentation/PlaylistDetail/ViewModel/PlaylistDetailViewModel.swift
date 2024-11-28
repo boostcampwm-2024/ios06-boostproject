a766fd6e6630e868c5b390a7a650f4b368da934b
kybeen <rei980627@gmail.com>
2024-11-28 18:25:01 +0900

    feat: 애플 뮤직 플레이리스트 내보내기 뷰모델 연결 (#176)
    - PlaylistDetailViewModel에 관련 메서드 작성

--- Molio/Source/Presentation/PlaylistDetail/ViewModel/PlaylistDetailViewModel.swift
+++ Molio/Source/Presentation/PlaylistDetail/ViewModel/PlaylistDetailViewModel.swift
@@ -3,22 +3,26 @@ import SwiftUI
 
 final class PlaylistDetailViewModel: ObservableObject {
     private let publishCurrentPlaylistUseCase: PublishCurrentPlaylistUseCase
-    private let checkAppleMusicSubscriptionUseCase : CheckAppleMusicSubscriptionUseCase
+    private let checkAppleMusicSubscriptionUseCase: CheckAppleMusicSubscriptionUseCase
+    private let exportAppleMusicPlaylistUseCase: ExportAppleMusicPlaylistUseCase
     private let musicKitService: MusicKitService // TODO: - 유즈케이스 분리
     
     @Published private(set) var isAppleMusicSubscriber: Bool = false
     @Published var currentPlaylist: MolioPlaylist?
     @Published var currentPlaylistMusics: [MolioMusic] = []
+    @Published var isExporting: Bool = false
     
     private var subscriptions: Set<AnyCancellable> = []
     
     init(
         publishCurrentPlaylistUseCase: any PublishCurrentPlaylistUseCase = DIContainer.shared.resolve(),
         checkAppleMusicSubscriptionUseCase: any CheckAppleMusicSubscriptionUseCase = DIContainer.shared.resolve(),
+        exportAppleMusicPlaylistUseCase: any ExportAppleMusicPlaylistUseCase = DIContainer.shared.resolve(),
         musicKitService: any MusicKitService = DIContainer.shared.resolve()
     ) {
         self.publishCurrentPlaylistUseCase = publishCurrentPlaylistUseCase
         self.checkAppleMusicSubscriptionUseCase = checkAppleMusicSubscriptionUseCase
+        self.exportAppleMusicPlaylistUseCase = exportAppleMusicPlaylistUseCase
         self.musicKitService = musicKitService
         
         bind()
@@ -42,6 +46,20 @@ final class PlaylistDetailViewModel: ObservableObject {
         Task { @MainActor [weak self] in
             let isSubscriber = try? await self?.checkAppleMusicSubscriptionUseCase.execute()
             self?.isAppleMusicSubscriber = isSubscriber ?? false
+        }
+    }
+    
+    func exportMolioPlaylistToAppleMusic() {
+        isExporting = true
+        guard let currentPlaylist = currentPlaylist else { return }
+        Task { @MainActor [weak self] in
+            do {
+                try await self?.exportAppleMusicPlaylistUseCase.execute(currentPlaylist)
+            } catch {
+                print(error.localizedDescription)
+                self?.isExporting = false
+            }
+            self?.isExporting = false
         }
     }
 }
