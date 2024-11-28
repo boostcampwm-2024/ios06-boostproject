351b8da3b46aded150f653aed6daccdc597b9deb
Park-kxng <coding.parkgy29@gmail.com>
2024-11-28 17:28:55 +0900

    feat: CurrentUserIdUseCase 프로토콜 및 구현체 정의 복구
    This reverts commit 9f04c1a22d345a37e25bcd7c845024c37f30baa0.

---
+++ Molio/Source/Domain/UseCase/UserUseCase/DefaultUserUseCase.swift
@@ -0,0 +1,78 @@
+import Foundation
+
+final class DefaultUserUseCase: UserUseCase {
+    private let service: UserService
+    
+    init(
+        service: UserService
+    ) {
+        self.service = service
+    }
+    
+    func createUser(userID: String, userName: String, imageURL: URL?, description: String?) async throws {
+        let newUser = MolioUserDTO(
+            id: userID,
+            name: userName,
+            profileImageURL: imageURL?.absoluteString,
+            description: description
+        )
+        try await service.createUser(newUser)
+    }
+    
+func fetchUser(userID: String) async throws -> MolioUser {
+    let userDTO = try await service.readUser(userID: userID)
+
+    let profileImageURL: URL?
+    if let urlString = userDTO.profileImageURL {
+        profileImageURL = URL(string: urlString)
+    } else {
+        profileImageURL = nil
+    }
+
+    let user = MolioUser(
+        id: userDTO.id,
+        name: userDTO.name,
+        profileImageURL: profileImageURL,
+        description: userDTO.description
+    )
+
+    return user
+}
+    
+    func updateUserName(userID: String, newName: String) async throws {
+        let user = try await service.readUser(userID: userID)
+        let newUser = MolioUserDTO(
+            id: user.id,
+            name: newName,
+            profileImageURL: user.profileImageURL,
+            description: user.description
+        )
+        try await service.updateUser(newUser)
+    }
+    
+    func updateUserDescription(userID: String, newDescription: String?) async throws {
+        let user = try await service.readUser(userID: userID)
+        let newUser = MolioUserDTO(
+            id: user.id,
+            name: user.name,
+            profileImageURL: user.profileImageURL,
+            description: newDescription
+        )
+        try await service.updateUser(newUser)
+    }
+    
+    func updateUserImage(userID: String, newImageURL: URL?) async throws {
+        let user = try await service.readUser(userID: userID)
+        let newUser = MolioUserDTO(
+            id: user.id,
+            name: user.name,
+            profileImageURL: newImageURL?.absoluteString,
+            description: user.description
+        )
+        try await service.updateUser(newUser)
+    }
+    
+    func deleteUser(userID: String) async throws {
+        try await service.deleteUser(userID: userID)
+    }
+}
