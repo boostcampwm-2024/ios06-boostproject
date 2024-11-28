873a1c1f4023fb391eb091622340015d1b70aa62
kybeen <rei980627@gmail.com>
2024-11-29 02:47:52 +0900

    refactor: ExportAppleMusicCompletionView -> ExportAppleMusicPlaylistView 이름 변경 및 뷰모델 연결 (#176)
    - PlaylistDetailViewModel과 연결

--- Molio/Source/Presentation/ExportPlaylist/View/ExportAppleMusicPlaylist/ExportAppleMusicPlaylistView.swift
+++ Molio/Source/Presentation/ExportPlaylist/View/ExportAppleMusicPlaylist/ExportAppleMusicPlaylistView.swift
@@ -1,12 +1,13 @@
 import SwiftUI
 
-struct ExportAppleMusicCompletionView: View {
-    private let playlistName: String
+struct ExportAppleMusicPlaylistView: View {
     private let playlistLabelRadius: CGFloat = 13
     private let playlistLabelBlur: CGFloat = 85
     
-    init(playlistName: String) {
-        self.playlistName = playlistName
+    @ObservedObject private var viewModel: PlaylistDetailViewModel
+    
+    init(viewModel: PlaylistDetailViewModel) {
+        self.viewModel = viewModel
     }
     
     var body: some View {
@@ -18,7 +19,7 @@ struct ExportAppleMusicCompletionView: View {
                 .padding(41)
             
             VStack(spacing: 5) {
-                Text.molioMedium(playlistName, size: 16)
+                Text.molioMedium(viewModel.currentPlaylist?.name ?? "molio 플레이리스트", size: 16)
                     .foregroundStyle(.white)
                     .padding(.vertical, 8)
                     .padding(.horizontal, 10)
@@ -27,23 +28,22 @@ struct ExportAppleMusicCompletionView: View {
                         Color.white.opacity(0.8).blur(radius: playlistLabelBlur)
                             .cornerRadius(playlistLabelRadius)
                     }
-                Text.molioSemiBold("플레이 리스트 생성 완료!", size: 21)
+                Text.molioSemiBold("플레이리스트 생성 완료!", size: 21)
                     .foregroundStyle(.white)
             }
             
             Spacer()
             
-            BasicButton(type: .goToAppleMusic) {
-                // TODO: - 애플 뮤직 이동
+            BasicButton(type: .confirm) {
+                
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding(.horizontal, 22)
         .padding(.bottom, 45)
-        .background(Color.background)
     }
 }
 
 #Preview {
-    ExportAppleMusicCompletionView(playlistName: "🎧 카공할 때 듣는 플리")
+    ExportAppleMusicPlaylistView(viewModel: PlaylistDetailViewModel())
 }
