119d4203cc086fa1324555ad187585125ab44344
Minsoo Kang <alstn38@naver.com>
2024-11-28 15:36:23 +0900

    feat: CommunityViewController Navigation Bar 구현 (#219)

--- Molio/Source/Presentation/Community/CommunityViewController.swift
+++ Molio/Source/Presentation/Community/CommunityViewController.swift
@@ -1,7 +1,30 @@
 import UIKit
 
 final class CommunityViewController: UIViewController {
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupNavigationBar()
+    }
+    
+    // MARK: - Private func
+    
+    private func setupNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "gearshape.fill"),
+            style: .plain,
+            target: self,
+            action: #selector(didTapSettingButton)
+        )
+    }
+    
+    // MARK: - objc func
+    
+    @objc private func didTapSettingButton() {
+        let settingViewModel = SettingViewModel()
+        let settingViewController = SettingViewController(viewModel: settingViewModel)
+        navigationController?.pushViewController(settingViewController, animated: true)
     }
 }
