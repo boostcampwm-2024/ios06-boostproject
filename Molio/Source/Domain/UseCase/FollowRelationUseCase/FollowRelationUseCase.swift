beb85e183412b9c583fc7d4cedf72bc48913e203
Park-kxng <coding.parkgy29@gmail.com>
2024-11-29 20:11:56 +0900

    refactor: DefaultFollowRelationUseCase 수정 (#139)

--- Molio/Source/Domain/UseCase/FollowRelationUseCase/FollowRelationUseCase.swift
+++ Molio/Source/Domain/UseCase/FollowRelationUseCase/FollowRelationUseCase.swift
@@ -3,8 +3,8 @@ protocol FollowRelationUseCase {
     func approveFollowing(relationID: String) async throws
     func refuseFollowing(relationID: String) async throws
     func unFollow(relationID: String) async throws
-    func fetchMyFollowingList() async throws -> [MolioFollowRelation]
-    func fetchFreindFollowingList(userID: String) async throws -> [MolioFollowRelation]
-    func fetchMyFollowerList() async throws -> [MolioFollowRelation]
-    func fetchFriendFollowerList(userID: String) async throws -> [MolioFollowRelation]
+    func fetchMyFollowingList() async throws -> [MolioUser]
+    func fetchFreindFollowingList(userID: String) async throws -> [MolioUser]
+    func fetchMyFollowerList() async throws -> [MolioFollower]
+    func fetchFriendFollowerList(userID: String) async throws -> [MolioFollower]
 }
