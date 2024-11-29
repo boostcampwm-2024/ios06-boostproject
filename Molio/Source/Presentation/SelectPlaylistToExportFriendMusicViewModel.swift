0851e7fd90cf21df5dec1629b5648df61e63fce8
windowcow <bzbzkang@gmail.com>
2024-11-30 02:13:19 +0900

    feat: SelectPlaylistToExportFriendMusicViewModel - 친구 노래를 저장할 내 플리를 선택하는 뷰에서 쓰일 뷰 모델

---
+++ Molio/Source/Presentation/SelectPlaylistToExportFriendMusicViewModel.swift
@@ -0,0 +1,52 @@
+import Combine
+import Foundation
+import SwiftUI
+
+final class SelectPlaylistToExportFriendMusicViewModel: ObservableObject {
+    @Published var playlists: [MolioPlaylist] = []
+    @Published var selectedPlaylist: MolioPlaylist?
+    
+    private let fetchAllPlaylistsUseCase: FetchAllPlaylistsUseCase
+    
+    private let manageMyPlaylistUseCase: ManageMyPlaylistUseCase
+    
+    let selectedMusic: MolioMusic
+    
+    init(
+        fetchAllPlaylistsUseCase: FetchAllPlaylistsUseCase = DefaultFetchAllPlaylistsUseCase(),
+        manageMyPlaylistUseCase: ManageMyPlaylistUseCase = DefaultManageMyPlaylistUseCase(),
+        
+        selectedMusic: MolioMusic
+    ) {
+        self.fetchAllPlaylistsUseCase = fetchAllPlaylistsUseCase
+        self.manageMyPlaylistUseCase = manageMyPlaylistUseCase
+        self.selectedMusic = selectedMusic
+        fetchPlaylists()
+    }
+    
+    func fetchPlaylists() {
+        Task { @MainActor [weak self] in
+            guard let playlists = await self?.fetchAllPlaylistsUseCase.execute() else { return }
+            self?.playlists = playlists
+        }
+    }
+    
+    func selectPlaylist(_ playlist: MolioPlaylist) {
+        Task { @MainActor [weak self] in
+            self?.selectedPlaylist = playlist
+        }
+    }
+    
+    func exportMusicToMyPlaylist (music: MolioMusic) {
+        guard let selectedPlaylist else { return }
+        
+        Task {
+            do {
+                try await manageMyPlaylistUseCase.addMusic(musicISRC: music.isrc, to: selectedPlaylist.id)
+            } catch {
+                print("내보내기 실패")
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
