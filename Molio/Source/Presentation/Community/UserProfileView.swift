a94f0b3236b0dc121841fbdfccf22dc5e6730124
Park-kxng <coding.parkgy29@gmail.com>
2024-11-29 15:14:11 +0900

    feat: UserProfileViewModel 추가 및 연결 (#203)

--- Molio/Source/Presentation/Community/UserProfileView.swift
+++ Molio/Source/Presentation/Community/UserProfileView.swift
@@ -1,111 +1,116 @@
 import SwiftUI
 
 struct UserProfileView: View {
-    private let playlistCount: Int = 2 // TODO: ViewModel 연결시 삭제
-    private let followingCount: Int = 10 // TODO: ViewModel 연결시 삭제
-    private let followerCount: Int = 20 // TODO: ViewModel 연결시 삭제
-    
+    @StateObject var viewModel: UserProfileViewModel
     let isMyProfile: Bool
     let followRelationType: FollowRelationType?
-    
-    @State var playlistsSample: [MolioPlaylist] = [
-        MolioPlaylist(id: UUID(), name: "카공할 때 듣는 플리", createdAt: Date(), musicISRCs: [], filter: MusicFilter(genres: [.pop, .rock, .jazz])),
-        MolioPlaylist(id: UUID(), name: "카공할 때 듣는 플리", createdAt: Date(), musicISRCs: [], filter: MusicFilter(genres: [.pop, .rock, .jazz])),
-        MolioPlaylist(id: UUID(), name: "카공할 때 듣는 플리", createdAt: Date(), musicISRCs: [], filter: MusicFilter(genres: [.pop, .rock, .jazz])),
-        MolioPlaylist(id: UUID(), name: "카공할 때 듣는 플리", createdAt: Date(), musicISRCs: [], filter: MusicFilter(genres: [.pop, .rock, .jazz])),
-        MolioPlaylist(id: UUID(), name: "카공할 때 듣는 플리", createdAt: Date(), musicISRCs: [], filter: MusicFilter(genres: [.pop, .rock, .jazz])),
-        MolioPlaylist(id: UUID(), name: "카공할 때 듣는 플리", createdAt: Date(), musicISRCs: [], filter: MusicFilter(genres: [.pop, .rock, .jazz])),
-        MolioPlaylist(id: UUID(), name: "카공할 때 듣는 플리", createdAt: Date(), musicISRCs: [], filter: MusicFilter(genres: [.pop, .rock, .jazz])),
-        MolioPlaylist(id: UUID(), name: "카공할 때 듣는 플리", createdAt: Date(), musicISRCs: [], filter: MusicFilter(genres: [.pop, .rock, .jazz])),
-        MolioPlaylist(id: UUID(), name: "카공할 때 듣는 플리", createdAt: Date(), musicISRCs: [], filter: MusicFilter(genres: [.pop, .rock, .jazz]))
-        
-    ]
+    let freindUserID: String?
     
     init(
         isMyProfile: Bool,
-        followRelationType: FollowRelationType? = nil
+        followRelationType: FollowRelationType? = nil,
+        viewModel: UserProfileViewModel,
+        freindUserID: String? = nil
     ) {
         self.isMyProfile = isMyProfile
         self.followRelationType = followRelationType
+        _viewModel = StateObject(wrappedValue: viewModel)
+        self.freindUserID = freindUserID
     }
     
     var body: some View {
-        VStack {
-            // MARK: - 상단 제목 및 기어 버튼
-            HStack {
-                Text("몰리오올리오")
-                    .font(.system(size: 20, weight: .semibold))
-                    .foregroundColor(.white)
-                Spacer()
-                
-                if isMyProfile {
-                    Button(action: {
-                        print("설정 버튼 클릭")
-                    }) {
-                        Image(systemName: "gearshape.fill")
-                            .foregroundStyle(.main)
-                    }
-                }
-            }
-            .padding(.horizontal, 22)
-            
-            // MARK: - 유저 정보 HStack
-            HStack {
-                Image(systemName: "person.circle")
-                    .resizable()
-                    .frame(width: 82, height: 82)
-                
-                GeometryReader { proxy in
-                    HStack(spacing: 10) {
-                        let size = CGSize(
-                            width: (proxy.size.width - 20) / 3,
-                            height: proxy.size.height
-                        )
-                        userInfoView(type: .playlist, value: playlistCount, size: size)
-                        userInfoView(type: .following, value: followingCount, size: size)
-                        userInfoView(type: .follower, value: followerCount, size: size)
-                    }
-                }
-                .frame(maxWidth: .infinity)
-            }
-            .frame(maxWidth: .infinity, maxHeight: 82)
-            .padding(.horizontal, 22)
-            .padding(.top, 16)
-            
-            Spacer().frame(height: 21)
-            
-            // MARK: - 유저 description
-            Text("몰리 덕후입니다. 플리 공유해요!")
-                .font(.system(size: 14, weight: .regular))
-                .foregroundColor(.white)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal, 22)
-            
-            Spacer().frame(height: 13)
-            
-            // MARK: - 팔로우 버튼
-            if !isMyProfile, let followRelationType = followRelationType {
-                FollowRelationButton(type: followRelationType)
-                    .padding(.horizontal, 22)
-                    .frame(height: 32)
-            }
-            
-            Spacer().frame(height: 13)
-            
-            // MARK: - 유저 플레이리스트
-            ScrollView(.vertical, showsIndicators: true) {
+        ZStack {
+            if viewModel.isLoading {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                    .scaleEffect(1.5)
+            } else {
                 VStack {
-                    ForEach(playlistsSample, id: \.self) { playlist in
-                        userPlaylistRowView(playlist: playlist)
+                    // MARK: - 상단 제목 및 기어 버튼
+                    HStack {
+                        Text("몰리오올리오")
+                            .font(.system(size: 20, weight: .semibold))
+                            .foregroundColor(.white)
+                        Spacer()
+                        
+                        if isMyProfile {
+                            Button(action: {
+                                print("설정 버튼 클릭")
+                            }) {
+                                Image(systemName: "gearshape.fill")
+                                    .foregroundStyle(.main)
+                            }
+                        }
                     }
+                    .padding(.horizontal, 22)
+                    
+                    // MARK: - 유저 정보 HStack
+                    HStack {
+                        Image(systemName: "person.circle")
+                            .resizable()
+                            .frame(width: 82, height: 82)
+                        
+                        GeometryReader { proxy in
+                            HStack(spacing: 10) {
+                                let size = CGSize(
+                                    width: (proxy.size.width - 20) / 3,
+                                    height: proxy.size.height
+                                )
+                                userInfoView(type: .playlist, value: viewModel.playlists.count, size: size)
+                                userInfoView(type: .following, value: viewModel.followings.count, size: size)
+                                userInfoView(type: .follower, value: viewModel.followers.count, size: size)
+                            }
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 82)
+                    .padding(.horizontal, 22)
+                    .padding(.top, 16)
+                    
+                    Spacer().frame(height: 21)
+                    
+                    // MARK: - 유저 description
+                    
+                    if viewModel.currentID != nil {
+                        Text("몰리 덕후입니다. 플리 공유해요!")
+                            .font(.system(size: 14, weight: .regular))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.horizontal, 22)
+                    }
+                
+                    Spacer().frame(height: 13)
+                    
+                    // MARK: - 팔로우 버튼
+                    if !isMyProfile, let followRelationType = followRelationType {
+                        FollowRelationButton(type: followRelationType)
+                            .padding(.horizontal, 22)
+                            .frame(height: 32)
+                    }
+                    
+                    Spacer().frame(height: 13)
+                    
+                    // MARK: - 유저 플레이리스트
+                    ScrollView(.vertical, showsIndicators: true) {
+                        VStack {
+                            ForEach(viewModel.playlists, id: \.self) { playlist in
+                                userPlaylistRowView(playlist: playlist)
+                            }
+                        }
+                        .padding(.horizontal, 22)
+                    }
+                    
+                    Spacer()
                 }
-                .padding(.horizontal, 22)
             }
-            
-            Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.background)
+        .onAppear {
+            Task {
+                try await viewModel.fetchData(isMyProfile: isMyProfile, friendUserID: nil)
+            }
+        }
     }
     
     func userPlaylistRowView(playlist: MolioPlaylist) -> some View {
@@ -144,18 +149,5 @@ struct UserProfileView: View {
         case playlist = "플레이리스트"
         case following = "팔로잉"
         case follower = "팔로워"
-    }
-}
-
-#Preview {
-    Group {
-        UserProfileView(isMyProfile: true)
-            .previewDisplayName("내 프로필")
-        
-        UserProfileView(isMyProfile: false, followRelationType: .following)
-            .previewDisplayName("다른 사용자 (팔로우)")
-        
-        UserProfileView(isMyProfile: false, followRelationType: .unfollowing)
-            .previewDisplayName("다른 사용자 (팔로우 안 함)")
     }
 }
