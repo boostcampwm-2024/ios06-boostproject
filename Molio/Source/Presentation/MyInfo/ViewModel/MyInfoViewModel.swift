8c9435ac7f2f991f75ad876d1d34927bddb6227c
Minsoo Kang <alstn38@naver.com>
2024-11-30 17:55:48 +0900

    feat: 내 정보 변경하기 현재 유저의 정보 값 반영 (#249)

--- Molio/Source/Presentation/MyInfo/ViewModel/MyInfoViewModel.swift
+++ Molio/Source/Presentation/MyInfo/ViewModel/MyInfoViewModel.swift
@@ -6,8 +6,23 @@ final class MyInfoViewModel: ObservableObject {
     let descriptionCharacterLimit: Int = 50
     
     @Published var userImageURL: URL?
-    @Published var userNickName: String = ""
-    @Published var userDescription: String = ""
+    @Published var userNickName: String
+    @Published var userDescription: String
+    
+    private let currentUserIdUseCase: CurrentUserIdUseCase
+    private let userUseCase: UserUseCase
+    
+    init(
+        currentUser: MolioUser,
+        currentUserIdUseCase: CurrentUserIdUseCase = DIContainer.shared.resolve(),
+        userUseCase: UserUseCase = DIContainer.shared.resolve()
+    ) {
+        self.userImageURL = currentUser.profileImageURL
+        self.userNickName = currentUser.name
+        self.userDescription = currentUser.description ?? ""
+        self.currentUserIdUseCase = currentUserIdUseCase
+        self.userUseCase = userUseCase
+    }
     
     var isPossibleNickName: Bool {
         userNickName.count <= nickNameCharacterLimit
