d8f090bf85a39a465be352bde8da4e7b0618ce65
kybeen <rei980627@gmail.com>
2024-12-02 05:34:23 +0900

    fix: 유저 검색 화면에서 화면 탭 시 키보드 내리도록 수정 (#294)
    - SwiftUI View+Extension에 키보드 내리는 메서드 추가

--- Molio/Source/Presentation/SearchUser/View/SearchUserView.swift
+++ Molio/Source/Presentation/SearchUser/View/SearchUserView.swift
@@ -56,6 +56,9 @@ struct SearchUserView: View {
                 secondaryButton: .cancel()
             )
         }
+        .onTapGesture {
+            hideKeyboard()
+        }
         .onAppear {
             Task {
                 await viewModel.fetchAllUsers()
