b8d2b999173f30ec5b086235bb0e33580113b5c8
Minsoo Kang <alstn38@naver.com>
2024-12-03 01:50:26 +0900

    feat: 로그인 유무에 따른 검색화면 분기처리 (#307)

--- Molio/Source/Presentation/SearchUser/View/SearchUserView.swift
+++ Molio/Source/Presentation/SearchUser/View/SearchUserView.swift
@@ -6,63 +6,71 @@ struct SearchUserView: View {
     @State private var selectedUser: MolioFollower?
     
     var didUserInfoCellTapped: ((MolioFollower) -> Void)?
+    var didTabLoginRequiredButton: (() -> Void)?
     
     var body: some View {
-        VStack(spacing: 10) {
-            SearchBar(
-                searchText: $viewModel.searchText,
-                placeholder: "나의 몰리 찾기",
-                tintColor: .gray
-            )
-            .padding(10)
-            
-            ScrollView {
-                ForEach(viewModel.searchedUser, id: \.id) { user in
-                    
-                    Button(action: {
-                        didUserInfoCellTapped?(user)
-                    }) {
-                        UserInfoCell(
-                            user: user,
-                            followRelationType: user.followRelation,
-                            isDescriptionVisible: false
-                        ) { followType in
-                            if followType == .following {
-                                // 언팔로우 시 Alert 표시
-                                selectedUser = user
-                                showAlert = true
-                            } else {
-                                Task {
-                                    await viewModel.updateFollowState(for: user, to: followType)
+        ZStack {
+            VStack(spacing: 10) {
+                SearchBar(
+                    searchText: $viewModel.searchText,
+                    placeholder: "나의 몰리 찾기",
+                    tintColor: .gray
+                )
+                .padding(10)
+                
+                ScrollView {
+                    ForEach(viewModel.searchedUser, id: \.id) { user in
+                        
+                        Button(action: {
+                            didUserInfoCellTapped?(user)
+                        }) {
+                            UserInfoCell(
+                                user: user,
+                                followRelationType: user.followRelation,
+                                isDescriptionVisible: false
+                            ) { followType in
+                                if followType == .following {
+                                    // 언팔로우 시 Alert 표시
+                                    selectedUser = user
+                                    showAlert = true
+                                } else {
+                                    Task {
+                                        await viewModel.updateFollowState(for: user, to: followType)
 
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
-        }
-        .background(Color.background)
-        .alert(isPresented: $showAlert) {
-            Alert(
-                title: Text("언팔로우 확인"),
-                message: Text("\(selectedUser?.name ?? "")님을 언팔로우하시겠습니까?"),
-                primaryButton: .destructive(Text("언팔로우")) {
-                    if let user = selectedUser {
-                        Task {
-                            await viewModel.updateFollowState(for: user, to: .following)
+            .background(Color.background)
+            .alert(isPresented: $showAlert) {
+                Alert(
+                    title: Text("언팔로우 확인"),
+                    message: Text("\(selectedUser?.name ?? "")님을 언팔로우하시겠습니까?"),
+                    primaryButton: .destructive(Text("언팔로우")) {
+                        if let user = selectedUser {
+                            Task {
+                                await viewModel.updateFollowState(for: user, to: .following)
+                            }
                         }
-                    }
-                },
-                secondaryButton: .cancel()
-            )
-        }
-        .onTapGesture {
-            hideKeyboard()
-        }
-        .onAppear {
-            Task {
-                await viewModel.fetchAllUsers()
+                    },
+                    secondaryButton: .cancel()
+                )
+            }
+            .onTapGesture {
+                hideKeyboard()
+            }
+            .onAppear {
+                Task {
+                    await viewModel.fetchAllUsers()
+                }
+            }
+            if viewModel.shouldShowLoginRequired() {
+                LoginRequiredView {
+                    didTabLoginRequiredButton?()
+                }
             }
         }
     }
