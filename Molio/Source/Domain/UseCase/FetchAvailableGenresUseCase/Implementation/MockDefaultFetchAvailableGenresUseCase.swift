99d97648b5fdc0ca1ddbb1aa8596f1399b2c8897
kybeen <rei980627@gmail.com>
2024-12-03 22:53:13 +0900

    fix: 필터 화면 modal sheet 방식으로 수정 (#331)
    - 내비게이션 바 직접 추가
    - 이벤트 처리 수정

--- Molio/Source/Domain/UseCase/FetchAvailableGenresUseCase/Implementation/MockDefaultFetchAvailableGenresUseCase.swift
+++ Molio/Source/Domain/UseCase/FetchAvailableGenresUseCase/Implementation/MockDefaultFetchAvailableGenresUseCase.swift
@@ -2,7 +2,10 @@ struct MockFetchAvailableGenresUseCase: FetchAvailableGenresUseCase {
     var musicGenreArrToReturn: [MusicGenre]
     
     init(
-        musicGenreArrToReturn: [MusicGenre] = ["Jazz", "K-Pop"]
+        musicGenreArrToReturn: [MusicGenre] = [
+            "음악", "얼터너티브", "블루스", "크리스천", "클래식", "컨트리", "댄스",
+            "일렉트로닉", "힙합/랩", "홀리데이", "재즈", "K-Pop", "어린이 음악"
+        ]
     ) {
         self.musicGenreArrToReturn = musicGenreArrToReturn
     }
--- Molio/Source/Presentation/MusicFilter/View/MusicFilterViewController.swift
+++ Molio/Source/Presentation/MusicFilter/View/MusicFilterViewController.swift
@@ -3,75 +3,54 @@ import SwiftUI
 // MARK: - Implementation
 
 final class MusicFilterViewController: UIHostingController<MusicFilterView> {
-    typealias PopCompletion = ([MusicGenre]) -> Void // 필터 화면 pop이후 수행할 동작
+    typealias DismissCompletion = ([MusicGenre]?) -> Void // 필터 화면 pop이후 수행할 동작
     
     weak var delegate: MusicFilterViewControllerDelegate?
-    private var onPopCompletion: PopCompletion
+    private var onDismissCompletion: DismissCompletion?
     
     // MARK: - Initializer
     
     init(
         viewModel: MusicFilterViewModel,
-        onPopCompletion: @escaping PopCompletion
+        onDismissCompletion: DismissCompletion?
     ) {
         delegate = viewModel
         let musicFilterView = MusicFilterView(viewModel: viewModel)
-        self.onPopCompletion = onPopCompletion
+        self.onDismissCompletion = onDismissCompletion
         super.init(rootView: musicFilterView)
+        
+        rootView.saveButtonTapAction = { [weak self] in
+            self?.didTapSaveButton()
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
-        onPopCompletion = { _ in }
+        onDismissCompletion = { _ in }
         super.init(coder: aDecoder)
     }
-    
-    // MARK: - Navigation Bar Item
-    
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.molioSemiBold(text: "내 필터 편집", size: 17)
-        label.textColor = .white
-        return label
-    }()
-    
-    private let saveButton = UIButton.molioRegular(text: "저장",
-                                                   size: 17,
-                                                   foregroundColor: .main)
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .background
-        
-        // TODO: - 뒤로가기 버튼 텍스트 숨기기
-        
-        navigationItem.titleView = titleLabel
-        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: saveButton)
-        
-        saveButton.addTarget(self, action: #selector(didTapSaveButton), for: .touchUpInside)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.navigationBar.isHidden = false
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.navigationBar.isHidden = true
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.onDismissCompletion?(nil)
     }
     
     // MARK: - Event
     
-    @objc func didTapSaveButton() {
+    private func didTapSaveButton() {
         Task {
             do {
                 guard let delegate = delegate else { return }
                 let updatedFilter = try await delegate.didSaveButtonTapped()
                 showAlertWithOKButton(title: "필터 수정이 완료되었습니다.") { [weak self] _ in
-                    self?.navigationController?.popViewController(animated: true)
-                    self?.onPopCompletion(updatedFilter)
+                    self?.dismiss(animated: true)
+                    self?.onDismissCompletion?(updatedFilter)
                 }
             } catch {
                 showAlertWithOKButton(title: "필터 수정에 실패했습니다", message: error.localizedDescription)
